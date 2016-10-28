45ee89be177617988a303987f644cc8c442664b3
Andres Ortiz <candresortizm@gmail.com>
2016-10-27 21:54:19 -0500

    avance bootcamp

---
+++ bootcamp.rb
@@ -0,0 +1,4 @@
+require "./instructor"
+
+instructor1=Instructor.new
+puts "#{instructor1.last_name} #{instructor1.first_name}: #{instructor1.nickname}"
---
+++ instructor.rb
@@ -0,0 +1,9 @@
+class Instructor
+  attr_accessor :first_name,:last_name,:nickname
+  def initialize(first_name,last_name,nickname)
+    @first_name = first_name
+    @last_name = last_name
+    @nickname = nickname
+  end
+
+end
