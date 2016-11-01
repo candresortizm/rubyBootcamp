92a1f528b0c62cec479d6f962a02fc68db23c694
Andres Ortiz <candresortizm@gmail.com>
2016-10-31 19:09:38 -0500

    bootcamp e instructor

--- bootcamp.rb
+++ bootcamp.rb
@@ -1,4 +1,20 @@
 require "./instructor"
 
-instructor1=Instructor.new
-puts "#{instructor1.last_name} #{instructor1.first_name}: #{instructor1.nickname}"
+instructor1=Instructor.new({
+  first_name:"Florez",
+  last_name:"Diego",
+  nickname:"Wepaaa!",
+  strength:"HTML, JS, CSS"
+})
+puts "#{instructor1.last_name} #{instructor1.first_name}: #{instructor1.nickname} #{instructor1.claim_your_strength}"
+
+instructor2=Instructor.new({
+  first_name:"Gomez",
+  last_name:"Jorge",
+  nickname:"Yonga",
+  strength:"RoR"
+  })
+puts "#{instructor2.last_name} #{instructor2.first_name}: #{instructor2.nickname} #{instructor2.claim_your_strength}"
+
+#instructor3=Instructor.new("Ordoñez","Camilo",nil)
+#puts "#{instructor3.last_name} #{instructor3.first_name}: #{instructor3.nickname}"
--- instructor.rb
+++ instructor.rb
@@ -1,9 +1,15 @@
 class Instructor
-  attr_accessor :first_name,:last_name,:nickname
-  def initialize(first_name,last_name,nickname)
-    @first_name = first_name
-    @last_name = last_name
-    @nickname = nickname
+  attr_accessor :first_name,:last_name,:nickname,:age,:strength
+
+  def initialize(params ={})
+    @first_name = params[:first_name] ? params[:first_name]: "no name"
+    @last_name = params[:last_name] ? params[:last_name]: "no lastname"
+    @nickname = params[:nickname] if params[:nickname]
+    @age = params[:age] ? params[:age]: 1000
+    @strength = params[:strength] ? params[:strength]: "be lazy"
   end
 
+  def claim_your_strength
+    "I like so much #{self.strength.upcase}"
+  end
 end
